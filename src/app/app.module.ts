1d0c017aeb7d606b47f6d498c875e6c9c5c89cf1
pratikshapatilsurge <sts174@surgetechinc.in>
2023-09-11 17:33:14 +0530

    11-09-2023

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ProposalFormComponent } from './components/proposal-form/proposal-form.
 import { MatCardModule } from '@angular/material/card';
 import { CheckoutComponent } from './components/checkout/checkout.component';
 import { InboxComponent } from './components/inbox/inbox.component';
+import {HttpClientModule} from '@angular/common/http'
 
 @NgModule({
   declarations: [
@@ -33,7 +34,8 @@ import { InboxComponent } from './components/inbox/inbox.component';
     FlexLayoutModule,
     FormsModule, 
     ReactiveFormsModule,     
-    MatCardModule
+    MatCardModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,18 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import {HttpClient} from '@angular/common/http'
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
+  
 })
-export class LoginComponent   {
+export class LoginComponent implements OnInit   {
 
-  constructor(private router: Router) { }
+  //var for storing data from result
+  dataList:any;
+
+  constructor(private httpClient: HttpClient) {
+    this.dataList=[];
+   }
+
+  ngOnInit(): void {
+    this.getData()
+  }
+
+  getData(){
+    //result is var which stores data that comes from this url (response)
+    this.httpClient.get('http://localhost:8080/read-json').subscribe((result:any)=>{
+        this.dataList = result;
+        console.log("result"+result.data);
+    })
+  }
 
   onLoginButtonClick(): void {
     // Navigate to the 'vehicle' path when the 'Login' button is clicked
-    this.router.navigate(['/vehicle']);
+    // this.router.navigate(['/vehicle']);
   }
 
 }
